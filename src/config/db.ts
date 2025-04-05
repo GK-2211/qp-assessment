b4d8b1356b224a7724e17eeea344e209534490f2
GK-2211 <garima.m.khatri@gmail.com>
2025-04-05 17:09:04 +0530

    feature: db connection successful!!

---
+++ src/config/db.ts
@@ -0,0 +1,26 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const checkDatabaseConnection = async () => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT NOW()');
+      client.release();
+      console.log('Database connection successful:', result.rows[0].now);
+      return true;
+    } catch (error: any) {
+      console.error('Database connection failed:', error.message);
+      return false;
+    }
+  };
+  
+  export { pool as default, checkDatabaseConnection };
